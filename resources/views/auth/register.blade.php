06dbc80dc7a823f7f10601ee7fdac53472263272
Don-Quixote-Onoda <jmnicolas4me@gmail.com>
2023-02-05 19:05:36 +0800

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,87 +51,11 @@
             <!-- Header -->
             <header class="main-header p-0" id="header">
                 <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
-                    <!-- Sidebar toggle button -->
-                    <button id="sidebar-toggler" class="sidebar-toggle">
-                        <span class="sr-only">Toggle navigation</span>
-                    </button>
-
-
-                    @if (\Request::route()->getName() == 'admin.dashboard')
-                        <span class="page-title">Admin Dashboard</span>
-                    @endif
-                    @if (\Request::route()->getName() == 'admin-user')
-                        <span class="page-title">User Management</span>
-                    @endif
+                   
 
 
 
-
-                    <div class="navbar-right ">
-
-                        <!-- search form -->
-                        <div class="search-form">
-                            <form action="index.html" method="get">
-                                <div class="input-group input-group-sm" id="input-group-search">
-                                    <input type="text" autocomplete="off" name="query" id="search-input"
-                                        class="form-control" placeholder="Search..." />
-                                    <div class="input-group-append">
-                                        <button class="btn" type="button">/</button>
-                                    </div>
-                                </div>
-                            </form>
-                            <ul class="dropdown-menu dropdown-menu-search">
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.html">Morbi leo risus</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.html">Dapibus ac facilisis in</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.html">Porta ac consectetur ac</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.html">Vestibulum at eros</a>
-                                </li>
-
-                            </ul>
-
-                        </div>
-
-                        <ul class="nav navbar-nav">
-                            <!-- User Account -->
-                            <li class="dropdown user-menu">
-                                <button class="dropdown-toggle nav-link" data-toggle="dropdown">
-                                    <span class="d-none d-lg-inline-block">John Doe</span>
-                                </button>
-                                <ul class="dropdown-menu dropdown-menu-right">
-                                    <li>
-                                        <a class="dropdown-link-item" href="user-profile.html">
-                                            <i class="mdi mdi-account-outline"></i>
-                                            <span class="nav-text">My Profile</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-link-item" href="user-account-settings.html">
-                                            <i class="mdi mdi-settings"></i>
-                                            <span class="nav-text">Account Setting</span>
-                                        </a>
-                                    </li>
-
-                                    <li class="dropdown-footer">
-                                        <a class="dropdown-link-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            <i class="mdi mdi-logout"></i> Log Out </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            class="d-none">
-                                            @csrf
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
+                    
                 </nav>
 
 
@@ -351,20 +275,22 @@
                 </div>
             </div>
 
-            <!-- Footer -->
-            <footer class="footer mt-auto">
-                <div class="copyright bg-white">
-                    <p>
-                        &copy; <span id="copy-year"></span> Copyright Mono Dashboard Bootstrap Template by <a
-                            class="text-primary" href="http://www.iamabdus.com/" target="_blank">Abdus</a>.
-                    </p>
-                </div>
-                <script>
-                    var d = new Date();
-                    var year = d.getFullYear();
-                    document.getElementById("copy-year").innerHTML = year;
-                </script>
-            </footer>
+           <!-- Footer -->
+           <footer class="footer mt-auto">
+            <div class="copyright bg-white">
+                <p>
+                    &copy; <span id="copy-year"></span> 
+                   
+                    Copyright VapePaMore.
+                </p>
+            </div>
+            <script>
+                toastr.success(response.message, "Success!");
+                var d = new Date();
+                var year = d.getFullYear();
+                document.getElementById("copy-year").innerHTML = year;
+            </script>
+        </footer>
 
         </div>
     </div>
